52bf60346c46bb0431ce91f020c22dcfd0877319
Evgeniy Sokolov <ewgraf@gmail.com>
2008-08-20 17:32:02 +0000

    fix cms files

--- modules/backend/content/controllers/ajax.admin.content.controller.php
+++
@@ -1,6 +0,0 @@
-<?php
-	class AjaxAdminContentController extends AdminContentController 
-	{
-	}
-
-?>
--- modules/backend/navigation/controllers/ajax.admin.navigation.controller.php
+++
@@ -1,6 +0,0 @@
-<?php
-	class AjaxAdminNavigationController extends AdminNavigationController 
-	{
-	}
-
-?>
