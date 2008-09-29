820bb86587bea69cb386216e4c91390437175992
Evgeniy Sokolov <ewgraf@gmail.com>
2008-09-29 03:19:33 +0000

    refactoring PageHeadController: get Title from database straight

--- modules/all/PageHeadController.class.php
+++ modules/all/PageHeadController.class.php
@@ -3,13 +3,34 @@
 
 	class PageHeadController extends Controller
 	{
+		public function importSettings($settings)
+		{
+			if(Cache::me()->hasTicketParams('page'))
+			{
+				$this->setCacheTicket(
+					Cache::me()->createTicket('page')->
+						setKey(Page::me()->getId(), __CLASS__, __FUNCTION__)
+				);
+			}
+			
+			return $this;
+		}
+		
 		public function getModel()
 		{
-			return array(
-				'title' => Page::me()->getTitle(),
-				'description' => Page::me()->getDescription(),
-				'keywords' => Page::me()->getKeywords()
+			$dbQuery = "
+				SELECT title, description, keywords
+				FROM " . Database::me()->getTable('PagesData') . "
+				WHERE
+					page_id = ?
+			";
+			
+			$dbResult = Database::me()->query(
+				$dbQuery,
+				array(Page::me()->getId())
 			);
+
+			return Database::me()->fetchArray($dbResult);
 		}
 	}
 ?>
