c1e1041a396980a7ffb46d4e01256bd9a96993f5
Evgeniy Sokolov <ewgraf@gmail.com>
2008-10-13 07:41:37 +0000

    add auth 401 controller

---
+++ controllers/Auth401Controller.class.php
@@ -0,0 +1,44 @@
+<?php
+	/* $Id$ */
+	
+	/**
+	 * @license http://opensource.org/licenses/gpl-3.0.html GPLv3
+	 * @author Evgeniy Sokolov <ewgraf@gmail.com>
+	 * @copyright Copyright (c) 2008, Evgeniy Sokolov
+	 * //FIXME: tested?
+	*/
+	class Auth401Controller extends ChainController
+	{
+		private $requiredRights = array('root');
+		
+		/**
+		 * @return ModelAndView
+		 */
+		public function handleRequest()
+		{
+			$result = parent::handleRequest();
+
+			if(!User::me()->getId() && isset($_SERVER['PHP_AUTH_USER']))
+				User::me()->login($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+			
+			if(
+				User::me()->getId()
+				&& 	array_intersect(
+					array_values(User::me()->getRights()),
+					$this->requiredRights
+				) == $this->requiredRights
+			)
+				return $result;
+			
+			return $this->unAuthorized('Enter you auth data', 'Need auth');
+		}
+		
+		private function unAuthorized($realm, $cancelMessage)
+		{
+			header('WWW-Authenticate: Basic realm="' . $realm . '"');
+			header('HTTP/1.0 401 Unauthorized');
+			echo $cancelMessage;
+			die();
+		}
+	}
+?>
