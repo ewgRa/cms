9319328d02ca3b3880b9e53a0ee299bb7e4c4c36
Evgeniy Sokolov <ewgraf@gmail.com>
2008-10-15 04:57:16 +0000

    fix renderPageHeadMedia - javascript files

--- view/php/all/renderPageHeadMedia.php
+++ view/php/all/renderPageHeadMedia.php
@@ -3,9 +3,19 @@
 	{
 		foreach($model['includeFiles'] as $file)
 		{
-	?>
-		<link rel="stylesheet" type="<?php echo $file['content-type']?>" href="<?php echo $file['path']?>" />
-	<?php
+			switch($file['content-type'])
+			{
+				case MimeContentTypes::TEXT_JAVASCRIPT:
+?>
+	<script type="<?php echo $file['content-type']?>" src="<?php echo $file['path']?>"></script>
+<?php
+				break;
+				default:
+?>
+	<link rel="stylesheet" type="<?php echo $file['content-type']?>" href="<?php echo $file['path']?>" />
+<?php
+				break;
+			}
 		}
 	}
 ?>
