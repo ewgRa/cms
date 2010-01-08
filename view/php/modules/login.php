3f910bfc3aacb18dc30e98cb2615c765577a6ba7
Evgeniy Sokolov <ewgraf@gmail.com>
2010-01-08 15:16:55 +0000

    add type

--- view/php/modules/login.php
+++ view/php/modules/login.php
@@ -11,7 +11,7 @@
 	<form method="<?php echo $model->get('source');?>">
 		<input type="hidden" name="backurl" value="<?php echo $backurl;?>" /><br />
 		Login: <input type="text" name="login" /><br />
-		Password: <input type="text" name="password" />
+		Password: <input type="password" name="password" />
 		<input type="submit" />
 	</form>
 <?php
