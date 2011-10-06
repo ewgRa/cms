b1100a19c340802771c18e4e7daa71777f67e2be
Evgeniy Sokolov <e.sokolov@co.wapstart.ru>
2011-10-06 14:03:58 +0600

    session controller security hash

--- classes/controllers/SessionController.class.php
+++ classes/controllers/SessionController.class.php
@@ -15,7 +15,26 @@
 			\ewgraFramework\ModelAndView $mav
 		) {
 			\ewgraFramework\Session::me()->relativeStart();
+
+			if (\ewgraFramework\Session::me()->isStarted()) {
+				if (!\ewgraFramework\Session::me()->has('securityHash')) {
+					\ewgraFramework\Session::me()->set(
+						'securityHash',
+						$this->compileSecurityHash($request)
+					);
+				} else if (
+					\ewgraFramework\Session::me()->get('securityHash')
+						!= $this->compileSecurityHash($request)
+				)
+					\ewgraFramework\Session::me()->destroy();
+			}
+
 			return parent::handleRequest($request, $mav);
+		}
+
+		private function compileSecurityHash(\ewgraFramework\HttpRequest $request)
+		{
+			return sha1($request->getRemoteIp().'-'.$request->getUserAgent());
 		}
 	}
 ?>
